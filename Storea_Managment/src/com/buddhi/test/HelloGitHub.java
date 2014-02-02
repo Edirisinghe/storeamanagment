fe8d7e462eb90313071c70f15452173e54a02575
Buddhi <edirisinghebuddhi@gmail.com>
2014-02-02 11:33:50 +0530

    Signed-off-by: Buddhi <edirisinghebuddhi@gmail.com> First imlementation on git hub hello

---
+++ Storea_Managment/src/com/buddhi/test/HelloGitHub.java
@@ -0,0 +1,13 @@
+package com.buddhi.test;
+
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello git hub !");
+
+	}
+
+}
